376a46869f8d690b6ef2c0629c18ef4184c191f2
js group <webdev.jsasia@gmail.com>
2024-09-23 11:44:12 +0530

    test

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Landing from "./pages/Landing";
 import AboutUs from './pages/AboutUs';
 import Services from './pages/Services';
+import WhyUs from './pages/WhyUs';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Landing />
       <AboutUs />
       <Services />
+      <WhyUs />
     </React.Fragment>
     
   )
---
+++ Frontend/src/pages/WhyUs.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FaArrowRight } from "react-icons/fa";
+import { MdDone } from "react-icons/md";
+
+
+
+function WhyUs() {
+  return (
+    <React.Fragment>
+        <main className='w-full h-screen flex justify-evenly  items-center'>
+        <div>
+            <img src="../src/assets/why us.jpeg" alt="" />
+        </div>
+        <div>
+            <h1 className='text-4xl font-semibold'>Why Us?</h1>
+            <p> <MdDone className='text-[#F3BA73]'/> Proven Track Record of Success</p>
+            <p> <MdDone className='text-[#F3BA73]'/> Tailored Solutions for Every Need</p>
+            <p> <MdDone className='text-[#F3BA73]'/> Client-Centered Approach</p>
+            <p> <MdDone className='text-[#F3BA73]'/> Multi-Language Babysitters for International Families</p>
+            <a className='text-[#F3BA73]' href="">Learn More <FaArrowRight /></a>
+        </div>
+        </main>
+       
+    </React.Fragment>
+  )
+}
+
+export default WhyUs
