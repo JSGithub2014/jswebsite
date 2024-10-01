1fed758bcb5794d8fe874d928d3f20f7fc205a63
js group <webdev.jsasia@gmail.com>
2024-10-01 11:38:10 +0530

    made changes in Ourstrength to prevent conflict

--- Frontend/src/pages/Ourstrength.jsx
+++ Frontend/src/pages/Ourstrength.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 // Import images directly
 import firstPoint from '../assets/first-point-1.png'; // Ensure this path is correct
 import secondPoint from '../assets/Second-point-1.png'; // Case-sensitive
-import thirdPoint from '../assets/third-point-1.png'; // Ensure this path is correct
+import thirdPoint from '../assets/Third-point-1.png'; // Ensure this path is correct
 import fourthPoint from '../assets/fourth-point-1.png'; // Ensure this path is correct
 
 const StrengthSection = () => {
