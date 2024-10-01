a4cde5dc7a25d28bb26dd17165d6644bc5573c06
js group <webdev.jsasia@gmail.com>
2024-10-01 14:02:48 +0530

    changed width of FAQ heading

--- Frontend/src/pages/Faqs.jsx
+++ Frontend/src/pages/Faqs.jsx
@@ -36,11 +36,13 @@ const Faqs = () => {
   };
 
   return (
-    <div className="max-w-5xl mx-auto p-8 w-full">
-      <h1 className='text-4xl md:text-5xl lg:text-4xl mb-5 font-thin text-center tracking-wider bg-gradient-to-r from-yellow-400 to-orange-400 w-full mt-10 py-2'>
-        <span className='heading-font tracking-wider'>Frequently </span>
-        <span className='heading-font text-[rgb(255,102,0)]'>Asked Questions</span>
-      </h1>
+    <React.Fragment>
+      <h1 className='text-4xl md:text-5xl lg:text-4xl w-full mb-5 font-thin text-center tracking-wider bg-gradient-to-r from-yellow-400 to-orange-400 mt-10 py-4'>
+  <span className='heading-font tracking-wider'>Frequently </span>
+  <span className='heading-font text-[rgb(255,102,0)]'>Asked Questions</span>
+</h1>
+<div className="max-w-5xl mx-auto p-8 w-full">
+      
 
       {Object.keys(faqData).map((category) => (
         <div key={category} className="mb-12">
@@ -88,6 +90,8 @@ const Faqs = () => {
         </div>
       ))}
     </div>
+    </React.Fragment>
+    
   );
 };
 
