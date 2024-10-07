6365ebfc3bc16902e0e618c5deb478a3c6f8c75d
js group <webdev.jsasia@gmail.com>
2024-10-07 14:30:54 +0530

    test

--- Backend/routes/contactRoute.js
+++ Backend/routes/contactRoute.js
@@ -5,6 +5,10 @@ const dbgr = require("debug")("development:contactRoute.js");
 
 // POST route to send email
 router.post("/", async (req, res) => {
+    // Set CORS headers for this specific route
+    res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
     const { name, email, message } = req.body;
 
     const transporter = nodemailer.createTransport({
