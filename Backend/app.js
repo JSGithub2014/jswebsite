f87b082fc754431bad113d9a60da534edb3ab6bc
js group <webdev.jsasia@gmail.com>
2024-10-07 14:10:04 +0530

    fixed cors error in local

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const dbgr = require("debug")("development:app.js");
