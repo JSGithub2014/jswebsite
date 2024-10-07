a48ccda3e678338f9bd1b4c96bcc13ef433a027e
js group <webdev.jsasia@gmail.com>
2024-10-07 11:03:49 +0530

    added vercel.json

--- Frontend/src/components/AchievementCard.jsx
+++ Frontend/src/components/AchievementCard.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 
 function AchievementCard({ imgSrc, title, description, achievementNumber }) {
   return (
-    <article 
-      className="bg-white shadow-lg rounded-lg p-8 m-2 flex flex-col" 
-      role="article" 
-      aria-labelledby={`achievement-${achievementNumber}`}
-    >
+    <article className="bg-white shadow-lg rounded-lg p-8 m-2 flex flex-col " role="article" aria-labelledby={`achievement-${achievementNumber}`}>
       {/* Image with alt text for SEO */}
       <img 
         src={imgSrc} 
@@ -16,26 +12,15 @@ function AchievementCard({ imgSrc, title, description, achievementNumber }) {
       />
 
       {/* Achievement number */}
-      <p 
-        className="text-base md:text-2xl font-extrabold text-left mt-2" 
-        id={`achievement-number-${achievementNumber}`}
-      >
-        {achievementNumber}
-      </p>
+      <p className="text-base md:text-2xl font-extrabold text-left mt-2" id={`achievement-number-${achievementNumber}`}>{achievementNumber}</p>
 
       {/* Title */}
-      <h2 
-        className="text-sm md:text-md font-medium text-left mt-1" 
-        id={`achievement-title-${achievementNumber}`}
-      >
+      <h2 className="text-sm md:text-md font-medium text-left mt-1" id={`achievement-title-${achievementNumber}`}>
         {title}
       </h2>
       
       {/* Description */}
-      <p 
-        className="text-gray-600 text-xs md:text-sm text-left mt-1 text-justify" 
-        aria-describedby={`achievement-description-${achievementNumber}`}
-      >
+      <p className="text-gray-600 text-xs md:text-sm text-left mt-1" aria-describedby={`achievement-description-${achievementNumber}`}>
         {description}
       </p>
     </article>
