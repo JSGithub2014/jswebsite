f96f963480fcbe7142d7d8b875b10dd3450b62c7
js group <webdev.jsasia@gmail.com>
2024-10-07 12:22:00 +0530

    changing frontend url

--- Backend/app.js
+++ Backend/app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8000;
 
 // CORS setup
 app.use(cors({
-    origin: "https://www.jsasia.net", // No trailing slash
+    origin: "https://jswebsite-jsgroups-projects.vercel.app", // No trailing slash
     methods: ["GET", "POST"],
     credentials: true,
 }));
