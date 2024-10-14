0d01aa51c7b0aa1d4861304c678afae34a88d896
js group <webdev.jsasia@gmail.com>
2024-10-14 15:35:27 +0530

    adjustment in hero section 5

--- Frontend/src/pages/Landing.jsx
+++ Frontend/src/pages/Landing.jsx
@@ -6,7 +6,7 @@ function Landing() {
   return (
     <div className='bg-[rgb(255,102,0)]'>
       <Navbar />
-      <main className='pt-16 lg:py-0'>
+      <main className='pt-16 lg:py-8'>
         <Hero />
       </main>
     </div>
